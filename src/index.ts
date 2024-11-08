9ff732363feff118539ee0d7d44fe95ae4718cf2
LuckyMhz.com <ambinintsoa.m.herizo@gmail.com>
2024-11-08 09:08:33 +0100

    commit with template

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
-import express from 'express';
+import express, { Application } from 'express';
+import path from 'path';
 import fileRoute from './routes/fileRoute';
 
-const app = express();
+const app: Application = express();
 const PORT = process.env.PORT || 3000;
+
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.json());
 app.use('/api', fileRoute);
 
+// Serve the upload form
+app.get('/', (req, res) => {
+    res.render('uploadForm');
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on <http://localhost>:${PORT}`);
 });
